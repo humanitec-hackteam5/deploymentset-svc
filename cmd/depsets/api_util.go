b18580e12018e6737d985c895335f8a8b00ab471
Chris Stephenson <chris.stephenson@humanitec.com>
2020-03-12 17:16:19 +0100

    Added JWT support for user identification.

--- cmd/depsets/api_util.go
+++ cmd/depsets/api_util.go
@@ -2,17 +2,51 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
+
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
+// HumanitecClaims represents the claims in the Humanitec provided JWT
+type HumanitecClaims struct {
+	jwt.StandardClaims
+	UserUUID string   `json:"user_uuid,omitempty"`
+	OrgUUIDs []string `json:"organization_uuids,omitempty"`
+	Username string   `json:"username,omitempty"`
+	Scope    string   `json:"scope,omitempty"`
+}
+
+// claimsFromJWT parses a JWT for the claims
+func claimsFromJWT(JWT string) (HumanitecClaims, error) {
+	parser := jwt.Parser{nil, false, true}
+	var claims HumanitecClaims
+	_, _, err := (&parser).ParseUnverified(JWT, &claims)
+	if err != nil {
+		return HumanitecClaims{}, fmt.Errorf("extracting humanitec claims from JWT: %w", err)
+	}
+	return claims, nil
+}
+
+// getUser gets the user from the supplied JWT. For testing without JWT, the "From" header can be used to supply the username.
 func getUser(r *http.Request) string {
-	if userName := r.Header.Get("from"); userName != "" {
+	if auth := r.Header.Get("authorization"); auth != "" {
+		if strings.HasPrefix(auth, "JWT") {
+			claims, err := claimsFromJWT(auth[4:])
+			if err == nil {
+				return claims.Username
+			}
+			log.Printf("getUser: %v", err)
+		}
+	} else if userName := r.Header.Get("from"); userName != "" {
 		return userName
 	}
 	return "UNKNOWN"
 }
 
+// writeAsJSON writes the supplied object to a response along with the status code.
 func writeAsJSON(w http.ResponseWriter, statusCode int, obj interface{}) {
 	jsonObj, err := json.Marshal(obj)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module humanitec.io/deploymentset-svc
 go 1.13
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/mock v1.4.0
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
